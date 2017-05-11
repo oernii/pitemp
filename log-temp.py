10d0b1bc59c1a323629d63d9a9a026ced0316d70
oernii <Ernest@Beinrohr.sk>
2017-05-11 18:49:14 +0000

    timestamp

--- log-temp.py
+++ log-temp.py
@@ -15,7 +15,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None and temperature is not None:
 	host="rtemp1"
-	data="temp_humidity,host={0:s} temp={1:0.1f},humidity={2:0.1f} {3:0.0f}".format(host, temperature, humidity, time.time())
+	data="temp_humidity,host={0:s} temp={1:0.1f},humidity={2:0.1f} {3:0.0f}000000004".format(host, temperature, humidity, time.time())
 	r = requests.post('http://192.168.1.100:8086/write?db=temp', data )
 else:
     print('Failed to get reading. Try again!')
